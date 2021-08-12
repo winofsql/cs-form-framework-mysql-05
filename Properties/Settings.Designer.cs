7328a35a46799ad278390a82259d8bbaa2e38c12
sworc <lightbox@nifty.com>
2021-08-12 13:51:57 +0900

    Update Settings.Designer.cs

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace cs_form_framework_mysql_03.Properties {
+namespace cs_form_framework_mysql_05.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
